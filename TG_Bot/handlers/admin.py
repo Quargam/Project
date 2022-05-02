da8ca57f5adc6c6a9a11e088dc8e338f90c86339
Quargam <kapylovmacim@mail.ru>
2022-05-02 09:14:17 +0300

    add sendall

--- TG_Bot/handlers/admin.py
+++ TG_Bot/handlers/admin.py
@@ -10,10 +10,13 @@ from data_base import sqlite_db, db
 
 db_users = db.Database_users("Users_db.db") # объект для управления users в БД
 
-class FSMAdmin(StatesGroup): # Машины состояний загрузки нового мероприятия
+class FSM_Admin(StatesGroup): # Машины состояний загрузки нового мероприятия
     photo = State()
     name = State()
     description = State()
+
+class FSM_Sendall(StatesGroup): # Машины состояний отправки всем сообщений
+    text = State()
 
 # Получить ID текущего модератора
 # @dp.message_handler(commands=['moderator'], is_chat_admin=True)
@@ -30,7 +33,7 @@ async def make_changes_command(message: types.Message):
 # @dp.message_handler(commands='Загрузить', state=None)
 async def cm_start(message: types.Message):
     if db_users.user_exists(message.from_user.id, 'admins'):
-        await FSMAdmin.photo.set()
+        await FSM_Admin.photo.set()
         await message.reply('Загрузи фото', reply_markup=admin_kb.button_case_admin_with_but_cancel)
 
 
@@ -39,7 +42,7 @@ async def cm_start(message: types.Message):
 async def load_photo(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['photo'] = message.photo[0].file_id
-    await FSMAdmin.next()
+    await FSM_Admin.next()
     await message.reply("Теперь введи название")
 
 
@@ -48,7 +51,7 @@ async def load_photo(message: types.Message, state: FSMContext):
 async def load_name(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['name'] = message.text
-    await FSMAdmin.next()
+    await FSM_Admin.next()
     await message.reply("Введи описание")
 
 
@@ -58,7 +61,7 @@ async def load_description(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['description'] = message.text
     await sqlite_db.sql_add_command(state)
-    await bot.send_message(message.from_user.id, 'команда успешно выполнена')
+    await bot.send_message(message.from_user.id, 'команда успешно выполнена',reply_markup=admin_kb.button_case_admin)
     await state.finish()
 
 # Выход из состояния
@@ -66,6 +69,7 @@ async def load_description(message: types.Message, state: FSMContext):
 async def cancel_handler(message: types.Message, state: FSMContext):
     current_state = await state.get_state()
     if current_state is None:
+        await message.reply('NOT OK', reply_markup=admin_kb.button_case_admin)
         return
     await state.finish()
     await message.reply('OK', reply_markup=admin_kb.button_case_admin)
@@ -89,13 +93,18 @@ async def delete_item(message: types.Message):
     await message.delete()
 
 # отправить сообщение всем пользователям в БД
-@dp.message_handler(commands=['sendall'])
+# @dp.message_handler(commands=['Рассылка'])
 async def command_sendall(message: types.Message):
+    if db_users.user_exists(message.from_user.id, 'admins'):
+        await FSM_Sendall.text.set()
+        await message.reply('Напишите текст который хотите всем отправить', reply_markup=admin_kb.button_case_admin_with_but_cancel)
+
+@dp.message_handler(state=FSM_Sendall.text)
+async def sendall(message: types.Message, state: FSMContext):
     if message.chat.type == 'private':
-        if db_users.user_exists(message.from_user.id, 'admins'):
-            text = message.text[9:]
+        text = message.text
+        if db_users.user_exists(message.from_user.id, 'admins') and text != '/Отмена':
             users = db_users.get_users(table='users')
-            print(users)
             for row in users:
                 try:
                     await bot.send_message(row[1], text, disable_notification=True)
@@ -103,15 +112,21 @@ async def command_sendall(message: types.Message):
                         db_users.set_active(row[1],active=1, table='users')
                 except:
                     db_users.set_active(row[1], active=0, table='users')
-            await bot.send_message(message.from_user.id, 'успешная рассылка')
+            await bot.send_message(message.from_user.id, 'успешная рассылка', reply_markup=admin_kb.button_case_admin)
+        elif text == '/Отмена':
+            await bot.send_message(message.from_user.id, 'Отмена', reply_markup=admin_kb.button_case_admin)
+    await state.finish()
     await message.delete()
+
+
 
 # команды к функциям
 def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(make_changes_command, commands=['moderator'], is_chat_admin=True)
     dp.register_message_handler(cancel_handler, state='*', commands='Отмена')
     dp.register_message_handler(cm_start, commands='Загрузить', state=None)
-    dp.register_message_handler(load_photo, content_types=['photo'], state=FSMAdmin.photo)
-    dp.register_message_handler(load_name, state=FSMAdmin.name)
-    dp.register_message_handler(load_description, state=FSMAdmin.description)
+    dp.register_message_handler(load_photo, content_types=['photo'], state=FSM_Admin.photo)
+    dp.register_message_handler(load_name, state=FSM_Admin.name)
+    dp.register_message_handler(load_description, state=FSM_Admin.description)
     dp.register_message_handler(delete_item, commands=['Удалить'])
+    dp.register_message_handler(command_sendall, commands=['Рассылка'])
--- TG_Bot/keyboards/admin_kb.py
+++ TG_Bot/keyboards/admin_kb.py
@@ -6,9 +6,11 @@ button_delete = KeyboardButton('/Удалить')
 button_cancel = KeyboardButton('/Отмена')
 button_back = KeyboardButton('/Назад')
 button_event = KeyboardButton('/Мероприятие')
+button_sendall = KeyboardButton('/Рассылка')
 
 #Клавиатура админа
-button_case_admin = ReplyKeyboardMarkup(resize_keyboard=True).add(button_load).add(button_delete).add(button_event)
+button_case_admin = ReplyKeyboardMarkup(resize_keyboard=True).add(button_load).add(button_delete).add(button_event).\
+add(button_sendall)
 
 #Клавиатура админа
 button_case_admin_with_but_cancel = ReplyKeyboardMarkup(resize_keyboard=True).add(button_cancel)
